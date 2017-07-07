6f8d81e7d4f7249578ed1e548246b20a1e31e2ed
Česlav Przywara <cp@bluechip.at>
2017-07-07 10:25:46 +0200

    Drop table prefix check from checklist. Also fix the comments a bit.

--- classes/BlueChip/Security/Checklist/AdminPage.php
+++ classes/BlueChip/Security/Checklist/AdminPage.php
@@ -29,24 +29,19 @@ class AdminPage extends \BlueChip\Security\Core\AdminPage
 		echo '<h1>' . $this->page_title . '</h1>';
 		echo '<p>The more <span class="dashicons dashicons-yes"></span> you have, the better!</p>';
 
-		echo '<table class="wp-list-table widefat">';
+		echo '<table class="wp-list-table widefat striped">';
 
-        echo '<tr class="">';
+        echo '<tr>';
 		$this->renderPhpFileEditationStatus();
         echo '</tr>';
 
-        echo '<tr class="alternate">';
-		$this->renderTablePrefixStatus();
-        echo '</tr>';
-
-        echo '<tr class="">';
+        echo '<tr>';
 		$this->renderNoObviousUsernamesStatus();
         echo '</tr>';
 
 		echo '</table>';
 
         echo '<p>';
-
         // TODO: How to escape text with link?
         echo sprintf(
             __('You might also want to enable some other <a href="%s">hardening options</a>.', 'bc-security'),
@@ -72,31 +67,14 @@ class AdminPage extends \BlueChip\Security\Core\AdminPage
 
 
 	/**
-	 * Render status info about non-default table prefix.
-	 * @global string $table_prefix
-	 */
-	private function renderTablePrefixStatus()
-    {
-		global $table_prefix;
-		//
-		$hardened = $table_prefix !== 'wp_';
-
-		echo '<th><span class="dashicons dashicons-' . ($hardened ? 'yes' : 'no') . '"></span></th>';
-		echo '<th>' . __('Non-default Table Prefix', 'bc-security') . '</th>';
-		echo '<td>' . sprintf(__('As an additional measure against blind SQL injections attacks, it is generally recommended to <a href="%1$s">change database tables prefix</a> to a non-default value, although there are <a href="%2$s">disagreeing opinions</a>.', 'bc-security'), 'https://codex.wordpress.org/Hardening_WordPress#Security_through_obscurity', 'https://www.wordfence.com/blog/2016/12/wordpress-table-prefix/') . '</td>';
-		echo '<td>' . esc_html__('Your table prefix is:', 'bc-security') . ' <em>' . $table_prefix . '</em></td>';
-	}
-
-
-	/**
 	 * Render status info about no obvious usernames being present on the system.
      *
-     * @filter bc_security_obvious_usernames Filters list of obvious usernames to check and report.
+     * @hook bc_security_status_obvious_usernames Filters list of obvious usernames to check and report.
 	 */
 	private function renderNoObviousUsernamesStatus()
     {
 		$obvious_usernames = apply_filters(Hooks::OBVIOUS_USERNAMES, ['admin', 'administrator']);
-		//
+
 		$ok = !\BlueChip\Security\Core\Utils::hasUsername($obvious_usernames);
 
 		echo '<th><span class="dashicons dashicons-' . ($ok ? 'yes' : 'no') . '"></span></th>';
