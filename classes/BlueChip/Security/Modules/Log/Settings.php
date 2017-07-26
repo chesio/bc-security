762914bdc7c338a5fb4735fd016020b91b92a9af
Česlav Przywara <cp@bluechip.at>
2017-07-26 17:29:29 +0200

    Implement interface to change log pruning configuration. Fixes #12.

--- classes/BlueChip/Security/Modules/Log/Settings.php
+++ classes/BlueChip/Security/Modules/Log/Settings.php
@@ -10,7 +10,7 @@ namespace BlueChip\Security\Modules\Log;
  */
 class Settings extends \BlueChip\Security\Core\Settings
 {
-    /** int: Maximum size of log table [20000] */
+    /** int: Maximum size of log table in thousands of records [20] */
     const LOG_MAX_SIZE = 'log_max_size';
 
     /** int: Maximum age of log record in days [365] */
@@ -28,9 +28,27 @@ class Settings extends \BlueChip\Security\Core\Settings
     {
         return [
             self::LOG_MAX_SIZE
-                => isset($s[self::LOG_MAX_SIZE]) ? intval($s[self::LOG_MAX_SIZE]) : 20000,
+                => isset($s[self::LOG_MAX_SIZE]) ? intval($s[self::LOG_MAX_SIZE]) : 20,
             self::LOG_MAX_AGE
                 => isset($s[self::LOG_MAX_AGE]) ? intval($s[self::LOG_MAX_AGE]) : 365,
         ];
+    }
+
+
+    /**
+     * @return int Maximum age of log records in seconds.
+     */
+    public function getMaxAge()
+    {
+        return $this->data[self::LOG_MAX_AGE] * DAY_IN_SECONDS;
+    }
+
+
+    /**
+     * @return int Maximum size of log table in number of records.
+     */
+    public function getMaxSize()
+    {
+        return $this->data[self::LOG_MAX_SIZE] * 1000;
     }
 }
