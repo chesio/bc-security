eb5fa30c7c75c4b3d9c9955768b02a1e811ef481
Česlav Przywara <cp@bluechip.at>
2018-03-21 11:47:45 +0100

    Add simple test for Modules\Cron\Job class.

---
+++ tests/src/Cases/Modules/Cron/JobTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package BC Security
+ */
+
+namespace BlueChip\Security\Tests\Cases\Modules\Cron;
+
+use BlueChip\Security\Modules\Cron;
+
+class JobTest extends \BlueChip\Security\Tests\TestCase
+{
+    const HOOK = 'test-job';
+
+    public function testScheduling()
+    {
+        // Create new job...
+        $job = new Cron\Job(self::HOOK, time(), 'daily');
+
+        // Constructing new job should not schedule it!
+        $this->assertFalse($job->isScheduled());
+        $this->assertFalse(wp_next_scheduled(self::HOOK));
+
+        // ...schedule it...
+        $job->schedule();
+
+        // ...test...
+        $this->assertTrue($job->isScheduled());
+        $this->assertInternalType('int', wp_next_scheduled(self::HOOK));
+
+        // ...and forward the job.
+        return $job;
+    }
+
+
+    /**
+     * @depends testScheduling
+     */
+    public function testUnscheduling(Cron\Job $job)
+    {
+        // Unschedule...
+        $job->unschedule();
+
+        // ...and test:
+        $this->assertFalse($job->isScheduled());
+        $this->assertFalse(wp_next_scheduled(self::HOOK));
+    }
+}
