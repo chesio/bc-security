chesio/bc-security

commit d699be23ded67b9eaea7d9bcfa09d3134a36b73b

Refactor event classes and internal handling.
Add some initial tests as well.

classes/BlueChip/Security/Modules/Log/Logger.php
@@ -97,7 +97,7 @@ class Logger extends Log\AbstractLogger implements Log\LoggerInterface, Modules\
         add_action(Action::INFO, [$this, 'info'], 10, 2);
         add_action(Action::DEBUG, [$this, 'debug'], 10, 2);
         add_action(Action::LOG, [$this, 'log'], 10, 3);
-        add_action(Action::EVENT, [$this, 'logEvent'], 10, 2);
+        add_action(Action::EVENT, [$this, 'logEvent'], 10, 1);
     }
 
 
@@ -123,7 +123,7 @@ class Logger extends Log\AbstractLogger implements Log\LoggerInterface, Modules\
             [
                 'date_and_time' => date(self::MYSQL_DATETIME_FORMAT, current_time('timestamp')),
                 'ip_address' => isset($context['ip_address']) ? $context['ip_address'] : $this->remote_address, // Allow overriding of IP address.
-                'event' => isset($context['event']) ? $context['event'] : '', // Event is optional.
+                'event' => $context['event'] ?? '', // Event is optional.
                 'level' => $this->translateLogLevel($level),
                 'message' => $message,
                 'context' => serialize($context),
@@ -141,15 +141,14 @@ class Logger extends Log\AbstractLogger implements Log\LoggerInterface, Modules\
 
 
     /**
-     * Log event with $event_id and $context.
+     * Log $event.
      *
-     * @param string $event_id
-     * @param array $context
+     * @param \BlueChip\Security\Modules\Log\Event $event
      */
-    public function logEvent($event_id, array $context)
+    public function logEvent(Event $event)
     {
-        $event = EventsManager::create($event_id);
-        $this->log($event->getLevel(), $event->getMessage(), array_merge(['event' => $event_id], $context));
+        // Include event ID in context.
+        $this->log($event->getLogLevel(), $event->getMessage(), array_merge(['event' => $event->getId()], $event->getContext()));
     }
 
 
@@ -159,7 +158,7 @@ class Logger extends Log\AbstractLogger implements Log\LoggerInterface, Modules\
      * @param string $level Log level constant: emergency, alert, critical, error, warning, notice, info or debug.
      * @return mixed Integer code for given log level or null, if unknown level given.
      */
-    public function translateLogLevel($level)
+    public function translateLogLevel(string $level)
     {
         switch ($level) {
             case Log\LogLevel::EMERGENCY:

classes/BlueChip/Security/Modules/Log/Event.php
@@ -5,94 +5,92 @@
 
 namespace BlueChip\Security\Modules\Log;
 
-class Event
+/**
+ * Base class for events. Implement event types as subclasses of this class.
+ */
+abstract class Event
 {
-    const AUTH_BAD_COOKIE = 'auth_bad_cookie';
-    const LOGIN_FAILURE = 'login_failure';
-    const LOGIN_LOCKOUT = 'login_lockdown';
-    const LOGIN_SUCCESSFUL = 'login_success';
-    const QUERY_404 = 'query_404';
-    const CORE_CHECKSUMS_VERIFICATION_ALERT = 'core_checksums_verification_alert';
-    const PLUGIN_CHECKSUMS_VERIFICATION_ALERT = 'plugin_checksums_verification_alert';
-
-
     /**
-     * @var string Unique ID
-     */
-    private $id;
-
-    /**
-     * @var string Human readable name
-     */
-    private $name;
-
-    /**
-     * @var string Log level
-     */
-    private $level;
-
-    /**
-     * @var string Log message
-     */
-    private $message;
-
-    /**
-     * @var array Required context keys
-     */
-    private $context;
-
-
-    /**
-     * Create event instance.
+     * @internal Static identifier is used for event type identification wherever use of classname is a bit cumbersome
+     * like in database data or GET requests.
      *
-     * @param string $id
-     * @param string $name
-     * @param string $level
-     * @param string $message
-     * @param array $context
+     * @return string Static identifier for event type (class).
      */
-    protected function __construct(string $id, string $name, string $level, string $message, array $context)
-    {
-        $this->id = $id;
-        $this->name = $name;
-        $this->level = $level;
-        $this->message = $message;
-        $this->context = $context;
-    }
-
-
     public function getId(): string
     {
-        return $this->id;
+        return static::ID;
     }
 
 
-    public function getName(): string
+    /**
+     * @return string Log level for event type (class).
+     */
+    public function getLogLevel(): string
     {
-        return $this->name;
+        return static::LOG_LEVEL;
     }
 
 
-    public function getLevel(): string
-    {
-        return $this->level;
-    }
+    /**
+     * @return string Human readable name unique for event type (class).
+     */
+    abstract public function getName(): string;
 
 
-    public function getMessage(): string
-    {
-        return $this->message;
-    }
+    /**
+     * @return string Log message providing context to the event.
+     */
+    abstract public function getMessage(): string;
 
 
+    /**
+     * @return array Context data for this event.
+     */
     public function getContext(): array
     {
-        return $this->context;
+        $reflection = new \ReflectionClass(static::class);
+
+        $output = [];
+        foreach ($reflection->getProperties(\ReflectionProperty::IS_PROTECTED) as $property) {
+            $output[$property->getName()] = $this->{$property->getName()};
+        }
+
+        return $output;
     }
 
 
-    public function hasContext(string $key): bool
+    /**
+     * @return array Context columns with human readable descriptions (labels).
+     */
+    public function explainContext(): array
     {
-        return isset($this->context[$key]);
+        $reflection = new \ReflectionClass(static::class);
+
+        $output = [];
+        foreach ($reflection->getProperties(\ReflectionProperty::IS_PROTECTED) as $property) {
+            $output[$property->getName()] = self::getPropertyLabel($property);
+        }
+
+        return $output;
+    }
+
+
+    /**
+     * Extract context property label.
+     *
+     * @internal Property label must be enclosed in pseudo-call to translation function __('I am the label') placed in
+     * property PHPDoc comment.
+     *
+     * @param \ReflectionProperty $property
+     * @return string
+     */
+    private static function getPropertyLabel(\ReflectionProperty $property): string
+    {
+        $matches = [];
+        if (preg_match("/__\('(.+)'\)/i", $property->getDocComment(), $matches)) {
+            return __($matches[1], 'bc-security');
+        } else {
+            return '';
+        }
     }
 }

classes/BlueChip/Security/Modules/Log/Events/AuthBadCookie.php
@@ -6,18 +6,42 @@
 namespace BlueChip\Security\Modules\Log\Events;
 
 use BlueChip\Security\Modules\Log\Event;
-use Psr\Log\LogLevel;
 
 class AuthBadCookie extends Event
 {
-    public function __construct()
+    /**
+     * @var string Static event identificator.
+     */
+    const ID = 'auth_bad_cookie';
+
+    /**
+     * @var string Event log level.
+     */
+    const LOG_LEVEL = \Psr\Log\LogLevel::NOTICE;
+
+    /**
+     * __('Username')
+     *
+     * @var string Username used in authentication with a bad cookie.
+     */
+    protected $username = '';
+
+
+    public function getName(): string
     {
-        parent::__construct(
-            self::AUTH_BAD_COOKIE,
-            __('Bad authentication cookie', 'bc-security'),
-            LogLevel::NOTICE,
-            __('Bad authentication cookie used with {username}.', 'bc-security'),
-            ['username' => __('Username', 'bc-security')]
-        );
+        return __('Bad authentication cookie', 'bc-security');
+    }
+
+
+    public function getMessage(): string
+    {
+        return __('Bad authentication cookie used with {username}.', 'bc-security');
+    }
+
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+        return $this;
     }
 }

classes/BlueChip/Security/Modules/Log/Events/CoreChecksumsVerificationAlert.php
@@ -6,18 +6,56 @@
 namespace BlueChip\Security\Modules\Log\Events;
 
 use BlueChip\Security\Modules\Log\Event;
-use Psr\Log\LogLevel;
 
 class CoreChecksumsVerificationAlert extends Event
 {
-    public function __construct()
+    /**
+     * @var string Static event identificator.
+     */
+    const ID = 'core_checksums_verification_alert';
+
+    /**
+     * @var string Event log level.
+     */
+    const LOG_LEVEL = \Psr\Log\LogLevel::WARNING;
+
+    /**
+     * __('Modified files')
+     *
+     * @var string List of modified core files found during check.
+     */
+    protected $modified_files = [];
+
+    /**
+     * __('Unknown files')
+     *
+     * @var string List of unknown files found during check.
+     */
+    protected $unknown_files = [];
+
+
+    public function getName(): string
     {
-        parent::__construct(
-            self::CORE_CHECKSUMS_VERIFICATION_ALERT,
-            __('Core checksums verification alert', 'bc-security'),
-            LogLevel::WARNING,
-            __('Following files have been modified: {modified_files}. Following files are unknown: {unknown_files}.', 'bc-security'),
-            ['modified_files' => __('Modified files', 'bc-security'), 'unknown_files' => __('Unknown files', 'bc-security')]
-        );
+        return __('Core checksums verification alert', 'bc-security');
+    }
+
+
+    public function getMessage(): string
+    {
+        return __('Following files have been modified: {modified_files}. Following files are unknown: {unknown_files}.', 'bc-security');
+    }
+
+
+    public function setModifiedFiles(array $modified_files): self
+    {
+        $this->modified_files = $modified_files;
+        return $this;
+    }
+
+
+    public function setUnknownFiles(array $unknown_files): self
+    {
+        $this->unknown_files = $unknown_files;
+        return $this;
     }
 }

classes/BlueChip/Security/Modules/Log/Events/LoginFailure.php
@@ -6,18 +6,42 @@
 namespace BlueChip\Security\Modules\Log\Events;
 
 use BlueChip\Security\Modules\Log\Event;
-use Psr\Log\LogLevel;
 
 class LoginFailure extends Event
 {
-    public function __construct()
+    /**
+     * @var string Static event identificator.
+     */
+    const ID = 'login_failure';
+
+    /**
+     * @var string Event log level.
+     */
+    const LOG_LEVEL = \Psr\Log\LogLevel::NOTICE;
+
+    /**
+     * __('Username')
+     *
+     * @var string Username used in failed login attempt.
+     */
+    protected $username = '';
+
+
+    public function getName(): string
     {
-        parent::__construct(
-            self::LOGIN_FAILURE,
-            __('Failed login', 'bc-security'),
-            LogLevel::NOTICE,
-            __('Login attempt with username {username} failed.', 'bc-security'),
-            ['username' => __('Username', 'bc-security')]
-        );
+        return __('Failed login', 'bc-security');
+    }
+
+
+    public function getMessage(): string
+    {
+        return __('Login attempt with username {username} failed.', 'bc-security');
+    }
+
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+        return $this;
     }
 }

classes/BlueChip/Security/Modules/Log/Events/LoginLockout.php
@@ -6,18 +6,70 @@
 namespace BlueChip\Security\Modules\Log\Events;
 
 use BlueChip\Security\Modules\Log\Event;
-use Psr\Log\LogLevel;
 
 class LoginLockout extends Event
 {
-    public function __construct()
+    /**
+     * @var string Static event identificator.
+     */
+    const ID = 'login_lockdown';
+
+    /**
+     * @var string Event log level.
+     */
+    const LOG_LEVEL = \Psr\Log\LogLevel::WARNING;
+
+    /**
+     * __('Duration')
+     *
+     * @var int Lockout duration (in seconds).
+     */
+    protected $duration = 0;
+
+    /**
+     * __('IP Address')
+     *
+     * @var string Remote IP address.
+     */
+    protected $ip_address = '';
+
+    /**
+     * __('Username')
+     *
+     * @var string Username used in failed login attempt.
+     */
+    protected $username = '';
+
+
+    public function getName(): string
     {
-        parent::__construct(
-            self::LOGIN_LOCKOUT,
-            __('Login lockout', 'bc-security'),
-            LogLevel::WARNING,
-            __('Remote IP address {ip_address} has been locked out from login for {duration} seconds. Last username used for login was {username}.', 'bc-security'),
-            ['ip_address' => __('IP Address', 'bc-security'), 'username' => __('Username', 'bc-security'), 'duration' => __('Duration', 'bc-security')]
-        );
+        return __('Login lockout', 'bc-security');
+    }
+
+
+    public function getMessage(): string
+    {
+        return __('Remote IP address {ip_address} has been locked out from login for {duration} seconds. Last username used for login was {username}.', 'bc-security');
+    }
+
+
+    public function setDuration(int $duration): self
+    {
+        $this->duration = $duration;
+        return $this;
+    }
+
+
+    public function setIpAddress(string $ip_address): self
+    {
+        $this->ip_address = $ip_address;
+        return $this;
+    }
+
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+        return $this;
     }
 }

classes/BlueChip/Security/Modules/Log/Events/LoginSuccessful.php
@@ -6,18 +6,42 @@
 namespace BlueChip\Security\Modules\Log\Events;
 
 use BlueChip\Security\Modules\Log\Event;
-use Psr\Log\LogLevel;
 
 class LoginSuccessful extends Event
 {
-    public function __construct()
+    /**
+     * @var string Static event identificator.
+     */
+    const ID = 'login_success';
+
+    /**
+     * @var string Event log level.
+     */
+    const LOG_LEVEL = \Psr\Log\LogLevel::INFO;
+
+    /**
+     * __('Username')
+     *
+     * @var string Username of user who logged in.
+     */
+    protected $username = '';
+
+
+    public function getName(): string
     {
-        parent::__construct(
-            self::LOGIN_SUCCESSFUL,
-            __('Successful login', 'bc-security'),
-            LogLevel::INFO,
-            __('User {username} logged in successfully.', 'bc-security'),
-            ['username' => __('Username', 'bc-security')]
-        );
+        return __('Successful login', 'bc-security');
+    }
+
+
+    public function getMessage(): string
+    {
+        return __('User {username} logged in successfully.', 'bc-security');
+    }
+
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+        return $this;
     }
 }

classes/BlueChip/Security/Modules/Log/Events/PluginChecksumsVerificationAlert.php
@@ -6,18 +6,84 @@
 namespace BlueChip\Security\Modules\Log\Events;
 
 use BlueChip\Security\Modules\Log\Event;
-use Psr\Log\LogLevel;
 
 class PluginChecksumsVerificationAlert extends Event
 {
-    public function __construct()
+    /**
+     * @var string Static event identificator.
+     */
+    const ID = 'plugin_checksums_verification_alert';
+
+    /**
+     * @var string Event log level.
+     */
+    const LOG_LEVEL = \Psr\Log\LogLevel::WARNING;
+
+    /**
+     * __('Plugin name')
+     *
+     * @var string Plugin name.
+     */
+    protected $plugin_name = '';
+
+    /**
+     * __('Plugin version')
+     *
+     * @var string Plugin version.
+     */
+    protected $plugin_version = '';
+
+    /**
+     * __('Modified files')
+     *
+     * @var array List of modified plugin files found during check.
+     */
+    protected $modified_files = [];
+
+    /**
+     * __('Unknown files')
+     *
+     * @var array List of unknown files found during check.
+     */
+    protected $unknown_files = [];
+
+
+    public function getName(): string
     {
-        parent::__construct(
-            self::PLUGIN_CHECKSUMS_VERIFICATION_ALERT,
-            __('Plugin checksums verification alert', 'bc-security'),
-            LogLevel::WARNING,
-            __('Plugin: {plugin_name} (ver. {plugin_version}). Following files have been modified: {modified_files}. Following files are unknown: {unknown_files}.', 'bc-security'),
-            ['plugin_name' => __('Plugin name', 'bc-security'), 'plugin_version' => __('Plugin version', 'bc-security'), 'modified_files' => __('Modified files', 'bc-security'), 'unknown_files' => __('Unknown files', 'bc-security')]
-        );
+        return __('Plugin checksums verification alert', 'bc-security');
+    }
+
+
+    public function getMessage(): string
+    {
+        return __('Plugin: {plugin_name} (ver. {plugin_version}). Following files have been modified: {modified_files}. Following files are unknown: {unknown_files}.', 'bc-security');
+    }
+
+
+    public function setPluginName(string $plugin_name): self
+    {
+        $this->plugin_name = $plugin_name;
+        return $this;
+    }
+
+
+    public function setPluginVersion(string $plugin_version): self
+    {
+        $this->plugin_version = $plugin_version;
+        return $this;
+    }
+
+
+    public function setModifiedFiles(array $modified_files): self
+    {
+        $this->modified_files = $modified_files;
+        return $this;
+    }
+
+
+    public function setUnknownFiles(array $unknown_files): self
+    {
+        $this->unknown_files = $unknown_files;
+        return $this;
     }
 }

commit 7980f8cc39f665e7eb0cbf4def168b01b5197a9f

Fix regression introduced in d699be23ded67b9eaea7d9bcfa09d3134a36b73b.

classes/BlueChip/Security/Modules/Log/Logger.php
@@ -335,7 +335,7 @@ class Logger extends Log\AbstractLogger implements Log\LoggerInterface, Modules\
     public function getKnownIps()
     {
         $result = $this->wpdb->get_results(
-            $this->wpdb->prepare("SELECT DISTINCT(ip_address) FROM {$this->log_table} WHERE event = %s", Event::LOGIN_SUCCESSFUL)
+            $this->wpdb->prepare("SELECT DISTINCT(ip_address) FROM {$this->log_table} WHERE event = %s", Events\LoginSuccessful::ID)
         );
 
         return is_array($result) ? wp_list_pluck($result, 'ip_address') : [];