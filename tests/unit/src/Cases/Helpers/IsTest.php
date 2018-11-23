5bc21534fbf5dae7e2876a1cf92bb59ec5cfdf7e
Česlav Przywara <cp@bluechip.at>
2018-11-23 14:23:05 +0100

    Refactor a bit.

--- tests/unit/src/Cases/Helpers/IsTest.php
+++ tests/unit/src/Cases/Helpers/IsTest.php
@@ -7,25 +7,26 @@ namespace BlueChip\Security\Tests\Unit\Cases\Helpers;
 
 use Brain\Monkey\Filters;
 use Brain\Monkey\Functions;
-use BlueChip\Security\Helpers;
+use BlueChip\Security\Helpers\Hooks;
+use BlueChip\Security\Helpers\Is;
 
 class IsTest extends \BlueChip\Security\Tests\Unit\TestCase
 {
     /**
-     * Ensure that `bc-security/filter:is-admin` fires when Helpers\Is::admin() method is invoked.
+     * Ensure that `bc-security/filter:is-admin` fires when Is::admin() method is invoked.
      */
     public function testIsAdminHookFires()
     {
         Functions\when('user_can')->justReturn(true);
 
-        Helpers\Is::admin(\Mockery::mock(\WP_User::class));
+        Is::admin(\Mockery::mock(\WP_User::class));
 
-        $this->assertEquals(1, Filters\applied(Helpers\Hooks::IS_ADMIN));
+        $this->assertEquals(1, Filters\applied(Hooks::IS_ADMIN));
     }
 
 
     /**
-     * Ensure that `bc-security/filter:is-admin` filters return value of Helpers\Is::admin().
+     * Ensure that `bc-security/filter:is-admin` filters return value of Is::admin().
      */
     public function testIsAdminFilter()
     {
@@ -34,19 +35,19 @@ class IsTest extends \BlueChip\Security\Tests\Unit\TestCase
         // User can't.
         Functions\when('user_can')->justReturn(false);
         // In: false; Out: false;
-        Filters\expectApplied(Helpers\Hooks::IS_ADMIN)->once()->with(false)->andReturn(false);
-        $this->assertFalse(Helpers\Is::admin($user));
+        Filters\expectApplied(Hooks::IS_ADMIN)->once()->with(false)->andReturn(false);
+        $this->assertFalse(Is::admin($user));
         // In: false; Out: true;
-        Filters\expectApplied(Helpers\Hooks::IS_ADMIN)->once()->with(false)->andReturn(true);
-        $this->assertTrue(Helpers\Is::admin($user));
+        Filters\expectApplied(Hooks::IS_ADMIN)->once()->with(false)->andReturn(true);
+        $this->assertTrue(Is::admin($user));
 
         // User can.
         Functions\when('user_can')->justReturn(true);
         // In: true; Out: false;
-        Filters\expectApplied(Helpers\Hooks::IS_ADMIN)->once()->with(true)->andReturn(false);
-        $this->assertFalse(Helpers\Is::admin($user));
+        Filters\expectApplied(Hooks::IS_ADMIN)->once()->with(true)->andReturn(false);
+        $this->assertFalse(Is::admin($user));
         // In: true; Out: true;
-        Filters\expectApplied(Helpers\Hooks::IS_ADMIN)->once()->with(true)->andReturn(true);
-        $this->assertTrue(Helpers\Is::admin($user));
+        Filters\expectApplied(Hooks::IS_ADMIN)->once()->with(true)->andReturn(true);
+        $this->assertTrue(Is::admin($user));
     }
 }
