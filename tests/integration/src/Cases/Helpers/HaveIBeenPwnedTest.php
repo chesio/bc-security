0d4d087fff77d85f20c77f57fe66369df8c0ad8b
Česlav Przywara <cp@bluechip.at>
2019-08-01 15:29:12 +0200

    Add integration test for password validation

--- tests/integration/src/Cases/Helpers/HaveIBeenPwnedTest.php
+++ tests/integration/src/Cases/Helpers/HaveIBeenPwnedTest.php
@@ -6,6 +6,7 @@
 namespace BlueChip\Security\Tests\Unit\Cases\Helpers;
 
 use BlueChip\Security\Helpers\HaveIBeenPwned;
+use BlueChip\Security\Tests\Integration\Constants;
 
 class HaveIBeenPwnedTest extends \BlueChip\Security\Tests\Integration\TestCase
 {
@@ -16,7 +17,7 @@ class HaveIBeenPwnedTest extends \BlueChip\Security\Tests\Integration\TestCase
      */
     public function testPwnedPassword()
     {
-        $this->assertTrue(HaveIBeenPwned::hasPasswordBeenPwned('123456')); // Definitely pwned password...
-        $this->assertFalse(HaveIBeenPwned::hasPasswordBeenPwned('This password has not been pwned... yet.'));
+        $this->assertTrue(HaveIBeenPwned::hasPasswordBeenPwned(Constants::PWNED_PASSWORD));
+        $this->assertFalse(HaveIBeenPwned::hasPasswordBeenPwned(Constants::SAFE_PASSWORD));
     }
 }
