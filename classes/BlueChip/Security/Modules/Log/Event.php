chesio/bc-security

commit d346c11d3b8084432e13aaa3d371bbdc2be15a9c

Add visibility to all class constants

classes/BlueChip/Security/Modules/Log/Event.php
@@ -10,12 +10,12 @@ abstract class Event
     /**
      * @var string Static event identificator.
      */
-    const ID = '';
+    protected const ID = '';
 
     /**
      * @var string Log level.
      */
-    const LOG_LEVEL = '';
+    protected const LOG_LEVEL = '';
 
     /**
      * @internal Static identifier is used for event type identification wherever use of classname is a bit cumbersome

classes/BlueChip/Security/Modules/Log/Events/AuthBadCookie.php
@@ -9,12 +9,12 @@ class AuthBadCookie extends Event
     /**
      * @var string Static event identificator.
      */
-    const ID = 'auth_bad_cookie';
+    protected const ID = 'auth_bad_cookie';
 
     /**
      * @var string Event log level.
      */
-    const LOG_LEVEL = \Psr\Log\LogLevel::NOTICE;
+    protected const LOG_LEVEL = \Psr\Log\LogLevel::NOTICE;
 
     /**
      * __('Username')

classes/BlueChip/Security/Modules/Log/Events/LoginFailure.php
@@ -9,12 +9,12 @@ class LoginFailure extends Event
     /**
      * @var string Static event identificator.
      */
-    const ID = 'login_failure';
+    protected const ID = 'login_failure';
 
     /**
      * @var string Event log level.
      */
-    const LOG_LEVEL = \Psr\Log\LogLevel::NOTICE;
+    protected const LOG_LEVEL = \Psr\Log\LogLevel::NOTICE;
 
     /**
      * __('Username')

classes/BlueChip/Security/Modules/Log/Events/LoginLockout.php
@@ -9,12 +9,12 @@ class LoginLockout extends Event
     /**
      * @var string Static event identificator.
      */
-    const ID = 'login_lockdown';
+    protected const ID = 'login_lockdown';
 
     /**
      * @var string Event log level.
      */
-    const LOG_LEVEL = \Psr\Log\LogLevel::WARNING;
+    protected const LOG_LEVEL = \Psr\Log\LogLevel::WARNING;
 
     /**
      * __('Duration')

classes/BlueChip/Security/Modules/Log/Events/LoginSuccessful.php
@@ -9,12 +9,12 @@ class LoginSuccessful extends Event
     /**
      * @var string Static event identificator.
      */
-    const ID = 'login_success';
+    protected const ID = 'login_success';
 
     /**
      * @var string Event log level.
      */
-    const LOG_LEVEL = \Psr\Log\LogLevel::INFO;
+    protected const LOG_LEVEL = \Psr\Log\LogLevel::INFO;
 
     /**
      * __('Username')

classes/BlueChip/Security/Modules/Log/Events/Query404.php
@@ -9,12 +9,12 @@ class Query404 extends Event
     /**
      * @var string Static event identificator.
      */
-    const ID = 'query_404';
+    protected const ID = 'query_404';
 
     /**
      * @var string Event log level.
      */
-    const LOG_LEVEL = \Psr\Log\LogLevel::INFO;
+    protected const LOG_LEVEL = \Psr\Log\LogLevel::INFO;
 
     /**
      * __('Request URI')

commit e08140fb30c6e864d1c2dad20799c564530c23ab

Fix regression introduced in d346c11d3b8084432e13aaa3d371bbdc2be15a9c

classes/BlueChip/Security/Modules/Log/Event.php
@@ -10,7 +10,7 @@ abstract class Event
     /**
      * @var string Static event identificator.
      */
-    protected const ID = '';
+    public const ID = '';
 
     /**
      * @var string Log level.

classes/BlueChip/Security/Modules/Log/Events/AuthBadCookie.php
@@ -9,7 +9,7 @@ class AuthBadCookie extends Event
     /**
      * @var string Static event identificator.
      */
-    protected const ID = 'auth_bad_cookie';
+    public const ID = 'auth_bad_cookie';
 
     /**
      * @var string Event log level.

classes/BlueChip/Security/Modules/Log/Events/LoginFailure.php
@@ -9,7 +9,7 @@ class LoginFailure extends Event
     /**
      * @var string Static event identificator.
      */
-    protected const ID = 'login_failure';
+    public const ID = 'login_failure';
 
     /**
      * @var string Event log level.

classes/BlueChip/Security/Modules/Log/Events/LoginLockout.php
@@ -9,7 +9,7 @@ class LoginLockout extends Event
     /**
      * @var string Static event identificator.
      */
-    protected const ID = 'login_lockdown';
+    public const ID = 'login_lockdown';
 
     /**
      * @var string Event log level.

classes/BlueChip/Security/Modules/Log/Events/LoginSuccessful.php
@@ -9,7 +9,7 @@ class LoginSuccessful extends Event
     /**
      * @var string Static event identificator.
      */
-    protected const ID = 'login_success';
+    public const ID = 'login_success';
 
     /**
      * @var string Event log level.

classes/BlueChip/Security/Modules/Log/Events/Query404.php
@@ -9,7 +9,7 @@ class Query404 extends Event
     /**
      * @var string Static event identificator.
      */
-    protected const ID = 'query_404';
+    public const ID = 'query_404';
 
     /**
      * @var string Event log level.