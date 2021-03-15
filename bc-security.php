64eaff5bac15df3cd98b40030239ceb53adfe5ed
Česlav Przywara <cp@bluechip.at>
2021-03-15 13:13:43 +0100

    Bump version to 0.17.0

--- bc-security.php
+++ bc-security.php
@@ -4,7 +4,7 @@
  * Plugin Name: BC Security
  * Plugin URI: https://github.com/chesio/bc-security
  * Description: Helps keeping WordPress websites secure.
- * Version: develop
+ * Version: 0.17.0
  * Author: Česlav Przywara <ceslav@przywara.cz>
  * Author URI: https://www.chesio.com
  * Requires PHP: 7.3
