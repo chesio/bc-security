538e2099efba4fae8d8e46c58f775582d25e317f
Česlav Przywara <cp@bluechip.at>
2023-10-19 12:10:02 +0200

    Use wp_admin_notice function in WP 6.4

--- tests/static-analysis/bootstrap.php
+++ tests/static-analysis/bootstrap.php
@@ -7,3 +7,6 @@ define('WP_PLUGIN_URL', '');
 define('AUTH_COOKIE', '');
 define('SECURE_AUTH_COOKIE', '');
 define('LOGGED_IN_COOKIE', '');
+
+// Upcoming in WordPress 6.4:
+function wp_admin_notice (string $message, array $args): void {}
