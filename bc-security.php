23d9b0b0954386ef69a48e9e78b34bac675fc131
Česlav Przywara <cp@bluechip.at>
2024-07-29 12:27:06 +0200

    Mark plugin as compatible with WordPress 6.6
    Fixes #157.

--- bc-security.php
+++ bc-security.php
@@ -9,7 +9,7 @@
  * Author URI: https://www.chesio.com
  * Requires PHP: 8.1
  * Requires at least: 6.4
- * Tested up to: 6.5
+ * Tested up to: 6.6
  * Text Domain: bc-security
  * GitHub Plugin URI: https://github.com/chesio/bc-security
  * Update URI: https://github.com/chesio/bc-security
