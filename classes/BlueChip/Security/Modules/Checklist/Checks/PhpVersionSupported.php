51d40c34837f83b9b4fd84a0e1169ef2a96122c0
Česlav Przywara <cp@bluechip.at>
2025-10-23 15:59:39 +0200

    Move PHP version related code to new helper

--- classes/BlueChip/Security/Modules/Checklist/Checks/PhpVersionSupported.php
+++ classes/BlueChip/Security/Modules/Checklist/Checks/PhpVersionSupported.php
@@ -4,23 +4,11 @@ declare(strict_types=1);
 
 namespace BlueChip\Security\Modules\Checklist\Checks;
 
+use BlueChip\Security\Helpers\PhpVersion;
 use BlueChip\Security\Modules\Checklist;
 
 class PhpVersionSupported extends Checklist\BasicCheck
 {
-    /**
-     * @var array<string,string> List of supported PHP versions and their end-of-life dates
-     *
-     * @link https://www.php.net/supported-versions.php
-     */
-    private const SUPPORTED_VERSIONS = [
-        '8.1' => '2025-12-31',
-        '8.2' => '2026-12-31',
-        '8.3' => '2027-12-31',
-        '8.4' => '2028-12-31',
-    ];
-
-
     public function getDescription(): string
     {
         return \sprintf(
@@ -39,68 +27,40 @@ class PhpVersionSupported extends Checklist\BasicCheck
 
     protected function runInternal(): Checklist\CheckResult
     {
-        // Get oldest supported version (as <major>.<minor>):
-        $oldest_supported_version = self::getOldestSupportedPhpVersion();
+        $phpVersionAsHtml = PhpVersion::getAsHtmlSnippet();
 
-        if (null === $oldest_supported_version) {
+        $isSupported = PhpVersion::isSupported();
+
+        if ($isSupported === null) {
             $message = \sprintf(
                 esc_html__('List of supported PHP versions is out-dated. Consider updating the plugin. Btw. you are running PHP %1$s.', 'bc-security'),
-                self::formatPhpVersion()
+                $phpVersionAsHtml
             );
             return new Checklist\CheckResult(null, $message);
         }
 
-        // Get active PHP version as <major>.<minor> string:
-        $php_version = \sprintf("%s.%s", PHP_MAJOR_VERSION, PHP_MINOR_VERSION);
-
-        if (\version_compare($php_version, $oldest_supported_version, '>=')) {
+        if ($isSupported) {
             // PHP version is supported, but do we have end-of-life date?
-            $eol_date = self::SUPPORTED_VERSIONS[$php_version] ?? null;
+            $eol_date = PhpVersion::getEndOfLifeDate();
             // Format message accordingly.
             $message = $eol_date
                 ? \sprintf(
                     esc_html__('You are running PHP %1$s, which is supported until %2$s.', 'bc-security'),
-                    self::formatPhpVersion(),
+                    $phpVersionAsHtml,
                     wp_date(get_option('date_format'), \strtotime($eol_date))
                 )
                 : \sprintf(
                     esc_html__('You are running PHP %1$s, which is still supported.', 'bc-security'),
-                    self::formatPhpVersion()
+                    $phpVersionAsHtml
                 )
             ;
             return new Checklist\CheckResult(true, $message);
         } else {
             $message = \sprintf(
                 esc_html__('You are running PHP %1$s, which is no longer supported! Consider upgrading your PHP version.', 'bc-security'),
-                self::formatPhpVersion()
+                $phpVersionAsHtml
             );
             return new Checklist\CheckResult(false, $message);
         }
-    }
-
-
-    /**
-     * @return string HTML tag with PHP version as <major>.<minor> string with full version in title attribute.
-     */
-    private static function formatPhpVersion(): string
-    {
-        return \sprintf('<em title="%s">%s.%s</em>', PHP_VERSION, PHP_MAJOR_VERSION, PHP_MINOR_VERSION);
-    }
-
-
-    /**
-     * @return string Oldest supported version of PHP as of today or null if it can not be determined from data available.
-     */
-    private static function getOldestSupportedPhpVersion(): ?string
-    {
-        $now = \time();
-
-        foreach (self::SUPPORTED_VERSIONS as $version => $eol_date) {
-            if (\strtotime($eol_date) >= $now) {
-                return $version;
-            }
-        }
-
-        return null;
     }
 }
